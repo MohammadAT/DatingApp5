7977c6e15e9797d6b25c5ec137aaa3a12cce77f6
MohammadAT <hmato.9@gmail.com>
2021-04-12 21:38:25 +0300

    add login and logout to BlazorApp ad members pages

---
+++ BlazorApp/Pages/Members/MemberCard.razor.cs
@@ -0,0 +1,15 @@
+﻿using BlazorApp.Models;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Pages.Members
+{
+    public partial class MemberCard
+    {
+        [Parameter]
+        public Member Member { get; set; }
+    }
+}
